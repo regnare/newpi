61a33b9ead046e0c201f8692461cc4535a3918fa
regnare <regnare@gmail.com>
2020-04-23 17:25:56 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 NEWHOST="newhostname"
 NEWDOMAIN="lan"
+NEWUSER="ben"
 
 NEWLOCALE="en_US.UTF-8"
 NEWLAYOUT="us"
@@ -10,11 +11,17 @@ NEWTIMEZONE="US/Eastern"
 sudo raspi-config nonint do_hostname "$NEWHOST"
 sudo raspi-config nonint do_change_locale "$NEWLOCALE"
 sudo raspi-config nonint do_configure_keyboard "$NEWLAYOUT"
-echo "$NEWTIMEZONE" | sudo tee /etc/timezone
-sudo dpkg-reconfigure -f noninteractive tzdata
+sudo raspi-config nonint do_wifi_country "$NEWLAYOUT"
 
-sudo sed -i 's/#host-name=foo/domain-name=$NEWHOST/g' /etc/avahi/avahi-daemon.conf
-sudo sed -i 's/#domain-name=local/domain-name=$NEWDOMAIN/g' /etc/avahi/avahi-daemon.conf
+sudo timedatectl set-timezone "$NEWTIMEZONE"
+
+sudo sed -i "s/#host-name=foo/domain-name=$NEWHOST/g" /etc/avahi/avahi-daemon.conf
+sudo sed -i "s/#domain-name=local/domain-name=$NEWDOMAIN/g" /etc/avahi/avahi-daemon.conf
 
 sudo apt update && sudo apt -y upgrade
 sudo apt -y install tmux vim zsh stow git 
+
+echo "Adding new user $NEWUSER"
+sudo useradd -m -s $(which zsh) -G users,sudo,adm "$NEWUSER"
+echo "Please set the password now."
+sudo passwd "NEWUSER"
