1f81c3086d3282727a64b50ed409d5e1d02b9643
regnare <regnare@gmail.com>
2020-04-23 17:32:14 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,11 @@ NEWLOCALE="en_US.UTF-8"
 NEWLAYOUT="us"
 NEWTIMEZONE="US/Eastern"
 
+echo "Adding new user $NEWUSER"
+sudo useradd -m -G users,sudo,adm "$NEWUSER"
+echo "Please set the password now."
+sudo passwd "$NEWUSER"
+
 sudo raspi-config nonint do_hostname "$NEWHOST"
 sudo raspi-config nonint do_change_locale "$NEWLOCALE"
 sudo raspi-config nonint do_configure_keyboard "$NEWLAYOUT"
@@ -21,7 +26,6 @@ sudo sed -i "s/#domain-name=local/domain-name=$NEWDOMAIN/g" /etc/avahi/avahi-dae
 sudo apt update && sudo apt -y upgrade
 sudo apt -y install tmux vim zsh stow git 
 
-echo "Adding new user $NEWUSER"
-sudo useradd -m -s $(which zsh) -G users,sudo,adm "$NEWUSER"
-echo "Please set the password now."
-sudo passwd "$NEWUSER"
+sudo usermod -s $(which zsh) "$NEWUSER"
+
+echo "Time to 'reboot'."
