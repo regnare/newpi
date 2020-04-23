c8698ca9133e077dd6d97cacb9c474ad062fe764
regnare <regnare@gmail.com>
2020-04-23 17:04:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NEWHOST="newhostname"
+NEWDOMAIN="lan"
 
 NEWLOCALE="en_US.UTF-8"
 NEWLAYOUT="us"
@@ -11,6 +12,9 @@ sudo raspi-config nonint do_change_locale "$NEWLOCALE"
 sudo raspi-config nonint do_configure_keyboard "$NEWLAYOUT"
 echo "$NEWTIMEZONE" | sudo tee /etc/timezone
 sudo dpkg-reconfigure -f noninteractive tzdata
+
+sudo sed -i 's/#host-name=foo/domain-name=$NEWHOST/g' /etc/avahi/avahi-daemon.conf
+sudo sed -i 's/#domain-name=local/domain-name=$NEWDOMAIN/g' /etc/avahi/avahi-daemon.conf
 
 sudo apt update && sudo apt -y upgrade
 sudo apt -y install tmux vim zsh stow git 
