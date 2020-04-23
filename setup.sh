4fdf9db80479b7064651e01692e7a26da8c9d8de
Ben Olson <regnare@gmail.com>
2020-04-23 16:30:57 -0400

    fixed /etc/timezone

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ NEWTIMEZONE="US/Eastern"
 sudo raspi-config nonint do_hostname "$NEWHOST"
 sudo raspi-config nonint do_change_locale "$NEWLOCALE"
 sudo raspi-config nonint do_configure_keyboard "$NEWLAYOUT"
-sudo echo "$NEWTIMEZONE" > /etc/timezone
+echo "$NEWTIMEZONE" | sudo tee /etc/timezone
 sudo dpkg-reconfigure -f noninteractive tzdata
 
 sudo apt update && sudo apt -y upgrade
