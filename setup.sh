5ac0a109e9230de5ecce8b49acf45fc149b91edc
regnare <regnare@gmail.com>
2020-04-23 17:46:13 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,11 +13,6 @@ sudo useradd -m -G users,sudo,adm "$NEWUSER"
 echo "Please set the password now."
 sudo passwd "$NEWUSER"
 
-sudo raspi-config nonint do_hostname "$NEWHOST"
-sudo raspi-config nonint do_change_locale "$NEWLOCALE"
-sudo raspi-config nonint do_configure_keyboard "$NEWLAYOUT"
-sudo raspi-config nonint do_wifi_country "$NEWLAYOUT"
-
 sudo timedatectl set-timezone "$NEWTIMEZONE"
 
 sudo sed -i "s/#host-name=foo/domain-name=$NEWHOST/g" /etc/avahi/avahi-daemon.conf
@@ -27,5 +22,10 @@ sudo apt update && sudo apt -y upgrade
 sudo apt -y install tmux vim zsh stow git 
 
 sudo usermod -s $(which zsh) "$NEWUSER"
+
+sudo raspi-config nonint do_hostname "$NEWHOST"
+sudo raspi-config nonint do_change_locale "$NEWLOCALE"
+sudo raspi-config nonint do_configure_keyboard "$NEWLAYOUT"
+sudo raspi-config nonint do_wifi_country "$NEWLAYOUT"
 
 echo "Time to 'reboot'."
