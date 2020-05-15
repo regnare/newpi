ae9b570af5d8a7618f3351f32833b0233391360b
Ben Olson <regnare@gmail.com>
2020-05-15 10:29:28 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -101,6 +101,9 @@ EOF
 
   # Update user shell as zsh
   sudo usermod -s $(which zsh) "$NEWUSER"
+  
+  # Configure nftables
+  source ./config-nft.sh
 
   # configure the locale settings
   sudo raspi-config nonint do_hostname "$NEWHOST"
